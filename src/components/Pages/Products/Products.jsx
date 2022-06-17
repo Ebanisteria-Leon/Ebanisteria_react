f9b38e835a5b9f7b24d13be1ea06a59a10d89c62
ItzMarlon2 <marlonamorteguicampo2@gmail.com>
2022-06-17 10:38:55 -0500

    avance del paginador

--- src/components/Pages/Products/Products.jsx
+++ src/components/Pages/Products/Products.jsx
@@ -59,7 +59,7 @@ export const Products = () => {
     const [categoria, setCategoria] = useState()
     const [tituCategoria, setTituCategoria] = useState("")
     const [pagina, setPagina] = useState(1)
-    const [porPagina, setPorPagina] = useState(15)
+    const [porPagina, setPorPagina] = useState(10)
     const maximo = todos.length / porPagina
     console.log(maximo);
 
--- src/components/UI/Paginacion/Paginacion.jsx
+++ src/components/UI/Paginacion/Paginacion.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import '../../../assets/css/Paginacion.css'
 
 export const Paginacion = ({pagina, setPagina, maximo}) => {
-    console.log(pagina);
-  return (
-    <div>
+     
+  let maximoSin = Math.trunc(maximo)
+  const [input, setInput] = useState(1)
+  
+  const nextPage= () =>{
+    setInput(input +1)
+    setPagina(pagina + 1)
+  }
 
+  return (
+    <div className="containerPaginator">
+      <button className="previusPage">
+        <i className="fa-solid fa-angle-left"></i>
+      </button>
+
+      <input name='page' autoComplete='off' value={input}/>
+      <p> de {maximoSin}</p>
+
+      <button className="nextPage" onClick={nextPage}>
+        <i className="fa-solid fa-angle-right"></i>
+      </button>
     </div>
-  )
+  ) 
 }
